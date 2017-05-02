910d069a827b2593eab089b94b89c58e6d612457
angelanmorlan <amorlan@vcd-ch204-06.eastern.ewu.edu>
2017-05-02 12:45:13 -0700

    add link button and fitText

--- js/scripts.js
+++ js/scripts.js
@@ -2,5 +2,7 @@ $('video').mediaelementplayer();
 $('h1').fitText(4);
 //$('.plot').fitText(5);
 $('h3').lettering('lines');
-$('.cast').fitText(6);
+$('.cast').fitText(5);
+$('body').fitVids(4);
 $('h2').fitText(4);
+$('#body').fitText(2);
